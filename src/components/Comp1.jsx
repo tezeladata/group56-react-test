98638f6dbca7cd859df5d1aeab661b2b22cb25ec
tezeladata <David.tezela@gmail.com>
2025-12-06 22:23:16 +0400

    Day 125 version 2

--- src/components/Comp1.jsx
+++ src/components/Comp1.jsx
@@ -1,26 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Comp1 = () => {
-  const [formData, setFormData] = useState({})
+  const [count, setCount] = useState(0);
 
-  const handleChange = ({target}) => {
-    setFormData(prev => ({
-      ...prev,
-      [target.name]: target.value
-    }))
-  }
+  useEffect(() => {
+    document.title = count
+  })
 
   return (
-    <>
-      <form>
-        <input type="text" name="username" placeholder="Enter username:" required onChange={handleChange} /> <br />
-
-        <input type="email" name="email" placeholder="Enter email:" required onChange={handleChange} /> <br />
-
-        <input type="password" name="password" placeholder="Enter password:" required onChange={handleChange} /> <br />
-      </form>
-    </>
+    <button onClick={() => setCount(prev => prev + 1)}>+1</button>
   )
 };
 
-export default Comp1;
+export default Comp1
