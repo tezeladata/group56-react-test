723b75a692e97cf68ad781bc06a563e0d0809d3a
tezeladata <David.tezela@gmail.com>
2025-12-06 21:36:47 +0400

    Day 125 version 1

--- src/components/Comp1.jsx
+++ src/components/Comp1.jsx
@@ -1,52 +1,25 @@
 import { useState } from "react";
 
-const allNames = ["bachi bediandze", "david", "soso rtveliashvili", "saba sabanshvili", "datuna sisauri", "biba", "giorgi jioshvili", "mate chubinidze"]
-
 const Comp1 = () => {
-  const [selectedArr, setSelectedArr] = useState([])
+  const [formData, setFormData] = useState({})
 
-  const addName = (el) => {
-    if (!selectedArr.includes(el)){
-      setSelectedArr(prev => [el, ...prev])
-    }
-  }
-
-  const deleteName = (el) => {
-    setSelectedArr(prev => [...prev].filter(username => username !== el))
+  const handleChange = ({target}) => {
+    setFormData(prev => ({
+      ...prev,
+      [target.name]: target.value
+    }))
   }
 
   return (
-    <section>
-      <div>
-        <h1>Selected names</h1>
+    <>
+      <form>
+        <input type="text" name="username" placeholder="Enter username:" required onChange={handleChange} /> <br />
 
-        <ul>
-          {
-            selectedArr.map((el, i) => (
-              <li key={i}>
-                {el}
-                <button onClick={() => deleteName(el)}>remove name</button>
-              </li>
-            ))
-          }
-        </ul>
+        <input type="email" name="email" placeholder="Enter email:" required onChange={handleChange} /> <br />
 
-        <button onClick={() => setSelectedArr(prev => [])}>Reset</button>
-      </div>
-
-      <hr />
-
-      <div>
-        <ul>
-          {allNames.map((el, i) => (
-            <li key={i}>
-              {el}
-              <button onClick={() => addName(el)}>add name</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
+        <input type="password" name="password" placeholder="Enter password:" required onChange={handleChange} /> <br />
+      </form>
+    </>
   )
 };
 
