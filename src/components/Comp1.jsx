fefdc20d72b683130d8dad78ae92485bcb3bcbd6
tezeladata <David.tezela@gmail.com>
2025-12-09 22:43:00 +0400

    Day 126 version 2

--- src/components/Comp1.jsx
+++ src/components/Comp1.jsx
@@ -1,20 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 const Comp1 = () => {
-    const [count, setCount] = useState(0);
+    const [intervalTime, setIntervalTime] = useState(null);
+    const [count, setCount] = useState(0)
 
-    const increment = () => setCount(prev => prev + 2);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setIntervalTime(prev => parseInt(e.target.timeInput.value))
+    }
 
     useEffect(() => {
-        console.log(count)
-    })
+        if (!intervalTime) return;
+
+        const intervalId = setInterval(() => {
+            setCount(prev => prev + 1)
+        }, intervalTime)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [intervalTime])
 
     return (
-        <>
-            <p>Count: {count}</p>
-            <button onMouseOver={increment}>Hover this button</button>
-        </> 
+        <div>
+            <p>Time: {count}</p>
+
+            <form onSubmit={handleSubmit}>
+                <input type="number" placeholder="Enter miliseconds" required name="timeInput" />
+                <button type="submit">submit</button>
+            </form>
+
+            <button onClick={() => setIntervalTime(prev => 0)}>Stop timer</button>
+            <button onClick={() => setCount(prev => 0)}>Reset time</button>
+        </div>
     )
 };
 
-export default Comp1;
+export default Comp1
