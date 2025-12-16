f409c0c663a92c1f90c62734657c925be40cc8f2
tezeladata <David.tezela@gmail.com>
2025-12-16 22:56:01 +0400

    Level 128, version 2

--- src/components/Comp1.jsx
+++ src/components/Comp1.jsx
@@ -1,37 +1,8 @@
-import { useEffect, useState } from "react";
-
 const Comp1 = () => {
-    const [users, setUsers] = useState([]);
-
-    const getInfo = async (apiLink) => {
-        try {
-            const res = await fetch(apiLink);
-            const data = await res.json();
-            setUsers(data)
-        } catch (error) {
-            console.error(error)
-        }
-    };
-
-    useEffect(() => {
-        getInfo("https://jsonplaceholder.typicode.com/users")
-    }, [])
-
     return (
-        <section>
-            <ul>
-                {
-                    users.map((user, ind) => (
-                        <li key={ind}>
-                            <p><b>Name:</b> {user.name}</p>
-                            <p><b>Phone:</b> {user.phone}</p>
-                            <p><b>Email:</b> {user.email}</p>
-                            <p><b>Website:</b> {user.website}</p>
-                        </li>
-                    ))
-                }
-            </ul>
-        </section>
+        <>
+        
+        </>
     )
 };
 
